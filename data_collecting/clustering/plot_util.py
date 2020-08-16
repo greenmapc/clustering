e7345d74bd1505140885848e021adf960c6dea7a
greenmapc <greenmapc@gmail.com>
2020-08-16 16:10:14 +0300

    updated plot

--- data_collecting/clustering/plot_util.py
+++ data_collecting/clustering/plot_util.py
@@ -3,5 +3,9 @@ import matplotlib.pyplot as plt
 
 def show_plot(data, x_name, y_name, labels):
     plt.figure(figsize=(10, 7))
+    plt.axis([0, data[x_name].max() * 1.1,
+              0, data[y_name].max() * 1.1])
+    plt.xlabel(x_name)
+    plt.ylabel(y_name)
     plt.scatter(data[x_name], data[y_name], c=labels)
     plt.show()
